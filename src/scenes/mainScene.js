8dbc7a65fe33b62e35a912b89c64cd010c62c8ab
unknown <cgcarlosg@hotmail.com>
2021-06-11 15:50:37 -0500

    Add preload

--- src/scenes/mainScene.js
+++ src/scenes/mainScene.js
@@ -1,4 +1,17 @@
 import Phaser from '../phaser';
+import player from '../../assets/images/player.png';
+import enemies from '../../assets/images/ships.png';
+import shield from '../../assets/images/Shield.png';
+import enemyLaser from '../../assets/images/LaserEnemy.png';
+import playerLaser from '../../assets/images/LaserPlayer.png';
+import explosion from '../../assets/images/Explosion.png';
+import exploSound from '../../assets/audio/soundExpl.wav';
+import laserSnd1 from '../../assets/audio/soundLaserPlayer.wav';
+import laserSnd2 from '../../assets/audio/soundLaserEnemy.wav';
+import { Player, PlayerLaser } from '../content/player';
+import { Enemy, EnemyLaser } from '../content/enemy';
+import { ShieldTile, Explosion } from '../content/entity';
+import Background from '../../assets/images/background.jpg';
 
 export default class MainScene extends Phaser.Scene {
     constructor() {
@@ -10,8 +23,24 @@ export default class MainScene extends Phaser.Scene {
     }
   
     preload() {
-    
-     
+      this.load.image('bgStart', Background);
+      this.load.image('sprPlayer', player);
+  
+      this.load.spritesheet('sprEnemy0', enemies, {
+        frameWidth: 8,
+        frameHeight: 8,
+      });
+      this.load.image('sprShieldTile', shield);
+      this.load.image('sprLaserEnemy', enemyLaser);
+      this.load.image('sprLaserPlayer', playerLaser);
+      this.load.spritesheet('sprExplosion', explosion, {
+        frameWidth: 8,
+        frameHeight: 8,
+      });
+  
+      this.load.audio('sndExplode', exploSound);
+      this.load.audio('sndLaserPlayer', laserSnd1);
+      this.load.audio('sndLaserEnemy', laserSnd2);
     }
   
     create() {
