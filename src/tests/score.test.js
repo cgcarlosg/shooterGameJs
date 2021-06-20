726178bc3710518229eb266687f8140d0e2f71fd
unknown <cgcarlosg@hotmail.com>
2021-06-20 00:12:04 -0500

    Fixed some tests

--- src/tests/score.test.js
+++ src/tests/score.test.js
@@ -8,36 +8,36 @@ describe('Posting Score to Api ', () => {
     fetch.resetMocks();
   });
 
-  test('Returns true if API call is successful', async () => {
+  test('true for successful API call', async () => {
     fetch.mockResponseOnce(JSON.stringify('Api test call'));
-    const result = await ScoreLogic.postScore('Ari', 100);
+    const result = await ScoreLogic.postScore('Carlos', 1200);
     expect(result).toBe(true);
   });
 
-  test('Returns true if API call is successful', async () => {
+  test('true for successful API call', async () => {
     fetch.mockResponseOnce(JSON.stringify('Api test call'));
-    const result = await ScoreLogic.postScore('Ari', 100);
+    const result = await ScoreLogic.postScore('Carlos', 1200);
     expect(result).not.toBe(false);
   });
 
-  test('Returns false if API call fails', async () => {
-    fetch.mockRejectOnce(new Error('fake error message'));
-    const result = await ScoreLogic.postScore('Ari', 100);
+  test('false if API call fails', async () => {
+    fetch.mockRejectOnce(new Error('error message'));
+    const result = await ScoreLogic.postScore('Carlos', 1200);
     expect(result).toBe(false);
   });
 
-  test('Returns false if API call fails to run', async () => {
-    fetch.mockRejectOnce(new Error('fake error message'));
-    const result = await ScoreLogic.postScore('Ari', 100);
+  test('false if API call fails', async () => {
+    fetch.mockRejectOnce(new Error('error message'));
+    const result = await ScoreLogic.postScore('Carlos', 1200);
     expect(result).not.toBe(true);
   });
 
   test('Posts to API with correct params', async () => {
     fetch.mockResponseOnce(JSON.stringify('Api test call'));
-    await ScoreLogic.postScore('Ari', 100);
+    await ScoreLogic.postScore('Carlos', 1200);
 
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch.mock.calls[0][1].body).toEqual(JSON.stringify({ user: 'Ari', score: 100 }));
+    expect(fetch.mock.calls[0][1].body).toEqual(JSON.stringify({ user: 'Carlos', score: 1200 }));
   });
 });
 
@@ -47,28 +47,28 @@ describe('Getting Scores from API', () => {
   });
 
   test('Returns an array of objects if API call is successful', async () => {
-    fetch.mockResponseOnce(JSON.stringify([{ user: 'Ari', score: 100 }]));
-    const scoreBoard = await ScoreLogic.getGameScores('Ari', 100);
+    fetch.mockResponseOnce(JSON.stringify([{ user: 'Carlos', score: 1200 }]));
+    const scoreBoard = await ScoreLogic.getGameScores('Carlos', 1200);
 
-    expect(scoreBoard).toEqual([{ user: 'Ari', score: 100 }]);
+    expect(scoreBoard).toEqual([{ user: 'Carlos', score: 1200 }]);
   });
 
   test('should math the score that been given', async () => {
-    fetch.mockResponseOnce(JSON.stringify([{ user: 'Ari', score: 100 }]));
-    const scoreBoard = await ScoreLogic.getGameScores('Ari', 100);
+    fetch.mockResponseOnce(JSON.stringify([{ user: 'Carlos', score: 1200 }]));
+    const scoreBoard = await ScoreLogic.getGameScores('Carlos', 1200);
     const data = scoreBoard;
     expect(data[0].score).toBeGreaterThan(5);
   });
 
   test('Returns error message if API call fails', async () => {
     fetch.mockRejectOnce(new Error('fake error message'));
-    const scoreBoard = await ScoreLogic.getGameScores('Ari', 100);
+    const scoreBoard = await ScoreLogic.getGameScores('Carlos', 1200);
     expect(scoreBoard).toBe('Sorry, something went wrong.');
   });
 
   test('Score should not be greater than give score', async () => {
-    fetch.mockResponseOnce(JSON.stringify([{ user: 'Ari', score: 10 }]));
-    const scoreBoard = await ScoreLogic.getGameScores('Ari', 100);
-    expect(scoreBoard[0].score).not.toBeGreaterThan(100);
+    fetch.mockResponseOnce(JSON.stringify([{ user: 'Carlos', score: 10 }]));
+    const scoreBoard = await ScoreLogic.getGameScores('Carlos', 1200);
+    expect(scoreBoard[0].score).not.toBeGreaterThan(1200);
   });
 });
